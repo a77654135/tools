1962798af600e20da119abdd583e03448a00ad50
talus <330243319@qq.com>
2018-02-27 15:51:23 +0800

    psd解析，生成exml文件功能，生成图片功能

--- convertPsd/convertPsd.py
+++ convertPsd/convertPsd.py
@@ -13,28 +13,602 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 import os
 import json
+import re
+import logging
+import shutil
+import configparser
 
-
-from common.ColorLogger import getUZWLogger
+from psdTools import *
 from psd_tools import PSDImage,Layer,Group
-
-logger = getUZWLogger()
-
+from common.ExceptCallStack import print_call_stack
 
 
 
 
+curPsdName = ""
+
+psdDir = ""
+exmlDir = ""
+generateExml = True
+replaceFile = False
+
+imgDir = ""
+generateImg = False
+generateLabelImg = False
+
+s9File = ""
+s9Map = {}
+
+smartSource = False
+defResFile = ""
+resNameMap = {}
 
 
 
+def configParser():
+    """
+    解析配置文件
+    :return:
+    """
+    global psdDir
+    global exmlDir
+    global imgDir
+    global generateExml
+    global generateImg
+    global generateLabelImg
+    global replaceFile
+    global s9File
+    global smartSource
+    global defResFile
+
+    parser = configparser.ConfigParser()
+    parser.read("config.ini",encoding="utf-8")
+
+    psdDir = os.path.abspath(parser.get("default", "psdDir"))
+    exmlDir = os.path.abspath(parser.get("default", "exmlDir"))
+    imgDir = os.path.abspath(parser.get("default", "imgDir"))
+    s9File = os.path.abspath(parser.get("default", "s9File"))
+    defResFile = os.path.abspath(parser.get("default", "defResFile"))
+
+    generateImg = parser.getboolean("default","generateImg")
+    generateLabelImg = parser.getboolean("default","generateLabelImg")
+    replaceFile = parser.getboolean("default","replaceFile")
+    generateExml = parser.getboolean("default","generateExml")
+    smartSource = parser.getboolean("default","smartSource")
 
 
+def readS9Info():
+    """
+    读取九宫格信息
+    :return:
+    """
+    global s9File
+    global s9Map
+    if os.path.exists(s9File):
+        with open(s9File, mode='r') as f:
+            s9Map = json.load(f, encoding="utf-8")
+
+def readResFile():
+    """
+    读取default.res.json
+    :return:
+    """
+    global smartSource
+    if not smartSource:
+        return
+
+    global defResFile
+    if not os.path.exists(defResFile):
+        return
+
+    with open(defResFile, mode='r') as f:
+        content = json.load(f, encoding="utf-8")
+        if content["resources"] is not None:
+            for res in content["resources"]:
+                parseSingleResource(res)
 
 
+def parseSingleResource(res):
+    """
+    解析组员组中的一个资源
+    :param res:
+    :return:
+    """
+    global resNameMap
+    if res["type"] == "sheet":
+        if res["subkeys"]:
+            for name in res["subkeys"].split(r","):
+                if resNameMap.has_key(name):
+                   resNameMap[name].append(res["name"])
+                else:
+                    resNameMap[name] = []
+                    resNameMap[name].append(res["name"])
+    elif res["type"] == "image":
+        if resNameMap.has_key(res["name"]):
+            resNameMap[res["name"]].append(None)
+        else:
+            resNameMap[res["name"]] = []
+            resNameMap[res["name"]].append(None)
+
+def walkDir(depth):
+    """
+    遍历文件夹
+    :param depth:
+    :return:
+    """
+    global psdDir
+    global exmlDir
+    global replaceFile
+
+    fromDir = os.path.join(psdDir,*depth)
+
+    for f in os.listdir(fromDir):
+        if f.startswith("unuse"):
+            continue
+
+        fromFile = os.path.join(fromDir,f)
+        if os.path.isdir(fromFile):
+            dp = depth[:]
+            dp.append(f)
+            walkDir(dp)
+        else:
+            ext = os.path.splitext(f)[-1]
+            if ext != ".psd":
+                continue
+
+            parsePsd(fromFile,depth)
+
+def parsePsd(fromFile,depth):
+    """
+    解析psd
+    :param fromFile:
+    :param toFile:
+    :return:
+    """
+
+    global curPsdName
+    curPsdName = os.path.split(fromFile)[1]
+
+    #print u"开始解析psd: {}".format(curPsdName)
+
+    global generateExml
+    global generateImg
+    if generateExml:
+        psdToExml(fromFile,depth)
+    if generateImg:
+        psdToImg(fromFile,depth)
+
+def psdToExml(fromFile,depth):
+    """
+    psd转换成exml
+    :param fromFile:
+    :param depth:
+    :return:
+    """
+
+    global exmlDir
+    global replaceFile
+
+    psdName = os.path.split(fromFile)[1]
+    name = psdName.split(r".")[0]
+
+    toFile = os.path.join(exmlDir,*depth)
+    if not os.path.exists(toFile):
+        os.makedirs(toFile)
+    toFile = os.path.join(toFile,psdName)
+    toFile = toFile.replace(r".psd",r"Skin.exml")
+
+    if os.path.exists(toFile) and not replaceFile:
+        return
+
+    psd = PSDImage.load(fromFile)
+
+    content = u""
+    content += u"<?xml version='1.0' encoding='utf-8'?>"
+    content += u'\n'
+    content += u'<e:Skin class="{}" width="{}" height="{}" xmlns:e="http://ns.egret.com/eui" xmlns:w="http://ns.egret.com/wing" xmlns:n="*">'.format(
+        name + "Skin", psd.header.width, psd.header.height)
+    content += u'\n'
+    content += groupToExml(psd, 0)
+    content += u'</e:Skin>'
+
+    if os.path.exists(toFile):
+        os.unlink(toFile)
+
+    try:
+        with open(toFile, mode="w+") as f:
+            f.write(content)
+        print u"生成 exml 成功：  " + toFile
+    except Exception, e:
+        print u"生成 exml 失败：  " + toFile
+
+def groupToExml(group, depth):
+    """
+    把图层组转成exml字符串
+    :param layer:
+    :param isRoot:
+    :return:
+    """
+    layers = group.layers
+    layers.reverse()
+
+    content = ""
+    for layer in layers:
+        if isLayerLocked(layer):
+            # 如果锁定了，不解析
+            continue
+        if isGroup(layer):
+            name = getLayerName(layer)
+            if name.startswith(r"$"):
+                content += specialGroupToExml(layer,depth+1)
+            else:
+                content += normalGroupToExml(layer,depth+1)
+        else:
+            if isLabel(layer):
+                content += labelToExml(layer, depth+1)
+            else:
+                content += layerToExml(layer, depth+1)
+    return content
+
+def labelToExml(layer,depth):
+    """
+    layer转成exml字符串
+    :param layer:
+    :return:
+    """
+
+    prefix = depth * "    "
+    content = ""
+    content += u'{}<e:Label '.format(prefix)
+
+    prop = getLayerProp(layer)
+    for k, v in prop.iteritems():
+        content += u'{0}="{1}" '.format(k, v)
+    content += u'/>'
+    content += u'\n'
+
+    return content
+
+def layerToExml(layer,depth):
+    """
+    layer转成exml字符串
+    :param layer:
+    :return:
+    """
+
+    global s9Map
+    global smartSource
+
+    prefix = depth * "    "
+    content = ""
+    content += u'{}<e:Image '.format(prefix)
+
+    prop = getLayerProp(layer)
+
+    #智能选择资源
+    src = prop.get("source","")
+    if src and smartSource:
+        src = getLayerSmartSrc(src)
+        prop["source"] = src
+
+    for k, v in prop.iteritems():
+        content += u'{0}="{1}" '.format(k, v)
+
+    name = getLayerName(layer)
+    s9Info = s9Map.get(name,None)
+    if s9Info:
+        content += u'scale9Grid="{}" '.format(s9Info)
+
+    content += u'/>'
+    content += u'\n'
+
+    return content
+
+def getLayerSmartSrc(sourceName):
+    """
+    获得智能资源名
+    :param src:
+    :return:
+    """
+    global curPsdName
+
+    currentPsdFile = curPsdName.split(r".")[0]
+
+    if resNameMap.has_key(sourceName):
+        folders = resNameMap[sourceName]
+        if currentPsdFile + "_json" in folders:
+            return currentPsdFile + r"_json." + sourceName
+        if "common_json" in folders:
+            return r"common_json." + sourceName
+        return sourceName
+
+    return sourceName
+
+def specialGroupToExml(layer,depth):
+    """
+    特殊组转成exml字符串
+    :param layer:
+    :return:
+    """
+    name = layer.name.strip()[1:]
+    if name.startswith("Button"):
+        return ButtonGroupToExml(layer,depth)
+    elif name.startswith("Bone"):
+        return BoneGroupToExml(layer,depth)
+    elif name.startswith("Skin"):
+        return SkinGroupToExml(layer,depth)
+    elif name.startswith("e_") or name.startswith("n_"):
+        return CommonGroupToExml(layer,depth)
+
+    return ""
+
+def ButtonGroupToExml(layer,depth):
+    """
+    Button组
+    :param layer:
+    :param depth:
+    :return:
+    """
+
+    prefix = depth * "    "
+    content = ""
+    content += u'{}<n:BaseButton '.format(prefix)
+
+    prop = getLayerProp(layer,True)
+
+    for k, v in prop.iteritems():
+        content += u'{0}="{1}" '.format(k, v)
+
+    content += u'>'
+    content += u'\n'
+
+    layers = layer.layers
+    layers.reverse()
+
+    for l in layers:
+        if isLayerLocked(l):
+            # 如果锁定了，不解析
+            continue
+        if isGroup(l):
+            name = getLayerName(l)
+            if name.startswith(r"$"):
+                content += specialGroupToExml(l,depth+1)
+            else:
+                content += normalGroupToExml(l,depth+1)
+        else:
+            if isLabel(l):
+                content += labelToExml(l, depth+1)
+            else:
+                content += layerToExml(l, depth+1)
+
+    content += u'{}</n:BaseButton>'.format(prefix)
+    content += u'\n'
+
+    return content
+
+def BoneGroupToExml(layer,depth):
+    """
+    龙骨组
+    :param layer:
+    :param depth:
+    :return:
+    """
+
+    prefix = depth * "    "
+    content = ""
+
+    content += u'{}<e:Group '.format(prefix)
+    prop = getLayerProp(layer)
+    prop["x"] = 0
+    prop["y"] = 0
+    prop["width"] = "100%"
+    prop["height"] = "100%"
+
+    for k, v in prop.iteritems():
+        content += u'{0}="{1}" '.format(k, v)
+    content += u'>'
+    content += u'\n'
+
+    layers = layer.layers
+    layers.reverse()
+
+    prefix2 = (depth + 1) * "    "
+    for l in layers:
+        name = getLayerName(l)
+        name = name.replace(r"_ske","")
+        attr = getLayerCustomProp(l)
+        x,y,_,__ = getLayerSize(l)
+
+        content += u'{}<n:BaseBone x="{}" y="{}" name="{}" '.format(prefix2,x,y,name)
+        for k, v in attr.iteritems():
+            content += u'{0}="{1}" '.format(k, v)
+        content += u'/>\n'
+
+    content += u'{}</e:Group>\n'.format(depth * u"    ")
+
+    return content
+
+
+def SkinGroupToExml(layer,depth):
+    """
+    psd层
+    :param layer:
+    :param depth:
+    :return:
+    """
+
+    prefix = depth * "    "
+    content = ""
+
+    layers = layer.layers
+    layers.reverse()
+
+    for l in layers:
+        name = getLayerName(l)
+        name += "Skin"
+        prop = getLayerProp(l)
+        if prop.has_key("source"):
+            del prop["source"]
+
+        content += u'{}<e:Panel '.format(prefix)
+        for k, v in prop.iteritems():
+            content += u'{0}="{1}" '.format(k, v)
+        content += u'skinName="{}" '.format(name)
+        content += u'/>\n'
+
+    return content
+
+
+def CommonGroupToExml(layer,depth):
+    """
+    自定义层
+    :param layer:
+    :param depth:
+    :return:
+    """
+
+    prefix = depth * "    "
+    content = ""
+
+    name = getLayerName(layer)
+    prop = getLayerCustomProp(layer)
+
+    if name.startswith("$e_"):
+        name = name.replace(r"$e_","")
+        content += u'{}<e:{} '.format(prefix,name)
+    else:
+        name = name.replace(r"$n_","")
+        content += u'{}<n:{} '.format(prefix,name)
+
+    for k, v in prop.iteritems():
+        content += u'{0}="{1}" '.format(k, v)
+    content += u'/>\n'
+
+    return content
+
+
+def normalGroupToExml(group,depth):
+    """
+    普通组转成exml字符串
+    :param layer:
+    :return:
+    """
+    prefix = depth * "    "
+    content = ""
+
+    content += u'{}<e:Group '.format(prefix)
+    prop = getLayerProp(group)
+    prop["x"] = 0
+    prop["y"] = 0
+    prop["width"] = "100%"
+    prop["height"] = "100%"
+
+    for k, v in prop.iteritems():
+        content += u'{0}="{1}" '.format(k, v)
+    content += u'>'
+
+    layers = group.layers
+    layers.reverse()
+
+    for layer in layers:
+        if isLayerLocked(layer):
+            # 如果锁定了，不解析
+            continue
+        if isGroup(layer):
+            name = getLayerName(layer)
+            if name.startswith(r"$"):
+                content += specialGroupToExml(group, depth + 1)
+            else:
+                content += normalGroupToExml(group, depth + 1)
+        else:
+            if isLabel(layer):
+                content += labelToExml(layer, depth + 1)
+            else:
+                content += layerToExml(layer, depth + 1)
+
+    content += u'{}</e:Group>'.format(prefix)
+    content += u'\n'
+    return content
+
+
+def psdToImg(fromFile,depth):
+    """
+    psd导出img
+    必须保证同名的图层是同一张图片
+    :param fromFile:
+    :param depth:
+    :return:
+    """
+    global imgDir
+
+    psdName = os.path.split(fromFile)[1]
+    name = psdName.split(r".")[0]
+
+    toFile = os.path.join(imgDir, *depth)
+    toFile = os.path.join(toFile,name)
+
+    if os.path.exists(toFile):
+        shutil.rmtree(toFile)
+    if not os.path.exists(toFile):
+        os.makedirs(toFile)
+
+    psd = PSDImage.load(fromFile)
+
+    groupToImg(psd,toFile)
+
+
+def groupToImg(group,imgDir):
+    """
+    把一个图层下面的
+    :param layer:
+    :return:
+    """
+    global generateLabelImg
+    for l in group.layers:
+        if isLayerLocked(l):
+            continue
+        if isGroup(l):
+            groupToImg(l,imgDir)
+        elif isLabel(l):
+            if generateLabelImg:
+                layerToImg(l,imgDir)
+        else:
+            layerToImg(l,imgDir)
+
+
+def layerToImg(layer,imgDir):
+    """
+    图层保存为png
+    :param layer:
+    :param imgDir:
+    :return:
+    """
+    idx = 1
+    if isLabel(layer):
+        name = "label_{}.png".format(idx)
+        while os.path.exists(os.path.join(imgDir,name)):
+            idx += 1
+            name = "label_{}.png".format(idx)
+    else:
+        name = getLayerName(layer) + ".png"
+
+    imgFile = os.path.join(imgDir,name)
+
+    if os.path.exists(imgFile):
+        return
+
+    layer_image = layer.as_PIL()
+    layer_image.save(imgFile)
 
 
 def main():
-    pass
+    try:
+        configParser()
+        readS9Info()
+        readResFile()
+        walkDir([])
+    except Exception,e:
+        print_call_stack()
 
 
 if __name__ == "__main__":
--- convertPsd/psdTools.py
+++ convertPsd/psdTools.py
@@ -18,6 +18,8 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 import os
 import json
+import re
+from collections import OrderedDict
 
 
 from common.ColorLogger import getUZWLogger
@@ -26,16 +28,361 @@ from psd_tools import PSDImage,Layer,Group
 logger = getUZWLogger()
 
 
+def rgbToHex(r,g,b):
+    """
+    rgb转十六进制
+    :param r:
+    :param g:
+    :param b:
+    :return:
+    """
+    r = hex(int(r))[2:]
+    g = hex(int(g))[2:]
+    b = hex(int(b))[2:]
+    r = "0" + r if len(r) < 2 else "" + r
+    g = "0" + g if len(g) < 2 else "" + g
+    b = "0" + b if len(b) < 2 else "" + b
+    return r"0x{}{}{}".format(r, g, b)
 
+def isLayer(layer):
+    """
+    是否是图层
+    :param layer:
+    :return:
+    """
+    return isinstance(layer, Layer) and layer.text_data is None
 
+def isLabel(layer):
+    """
+    是否是label
+    :param layer:
+    :return:
+    """
+    return isinstance(layer, Layer) and layer.text_data is not None
 
+def isGroup(layer):
+    """
+    是否是图层组
+    :param layer:
+    :return:
+    """
+    return isinstance(layer,Group)
+
+def getPsdSize(psd):
+    """
+    获得PSD的宽高
+    :param psd:
+    :return:
+    """
+    return int(psd.header.width),int(psd.header.height)
+
+def getLayerName(layer):
+    """
+    获得layer的名字
+    name 副本 :k=v;k=v;
+    :param layer:
+    :return:
+    """
+    return layer.name.split(r":")[0].split(r" ")[0].strip()
+
+def getLayerVisible(layer):
+    """
+    图层是否可见
+    :param layer:
+    :return:
+    """
+    return layer.visible
+
+def getLayerAlpha(layer):
+    """
+    获取alpha
+    :param layer:
+    :return:
+    """
+    try:
+        opacity = layer.opacity
+        if opacity == 255:
+            return 1
+        else:
+            return round(opacity/255.0,2)
+    except:
+        return 1
+
+def getLayerSize(layer):
+    """
+    获取图层位置大小
+    :param layer:
+    :return:
+    """
+    try:
+        box = layer.bbox
+        return int(box.x1), int(box.y1), int(box.width), int(box.height)
+    except Exception, e:
+        return 0, 0, 0, 0
+
+def isLayerLocked(layer):
+    """
+    图层是否锁定
+    :param layer:
+    :return:
+    """
+    assert isinstance(layer,Layer) or isinstance(layer,Group)
+    locked = False
+    try:
+        locked = layer._info[9][0]
+    except:
+        pass
+    return locked
+
+def parseList(lst):
+    hasKh = False
+    for l in lst:
+        if l.strip() == r"[" or l.strip() == r"]":
+            hasKh = True
+            break
+    if hasKh:
+        nl = []
+        inKh = False
+        for l in lst:
+            if l.strip() == r"[" and inKh == False:
+                inKh = True
+                continue
+            if inKh and l.strip() == r"]":
+                inKh = False
+                break
+            if inKh:
+                nl.append(l)
+        return nl
+    else:
+        return " ".join(lst)
+
+def parseEngineData(layer):
+    """
+    解析图层原始数据
+    :param layer:
+    :return:
+    """
+    try:
+        TySh = layer._tagged_blocks["TySh"][9][2]
+        for tp in TySh:
+            if tp[0] == "EngineData":
+                engineData = tp[1][0]
+                break
+        if isinstance(engineData,str) and engineData != "":
+            lines = engineData.splitlines()
+            newLines = []
+            for line in lines:
+                ln = line.strip()
+                if ln == "":
+                    continue
+                newLines.append(ln)
+
+            ed = {}
+            stack = [ed]
+            for idx, ln in enumerate(newLines):
+                stackLen = len(stack)
+                if stackLen <= 0:
+                    break
+                lastData = stack[stackLen - 1]
+
+                if ln == "<<":
+                    continue
+                if ln.startswith(r"/") and len(ln.split(r" ")) == 1 and newLines[idx + 1] == "<<":
+                    key = ln.lstrip(r"/")
+                    data = {}
+                    lastData[key] = data
+                    stack.append(data)
+                if ln.startswith(r"/") and len(ln.split(r" ")) > 1:
+                    lnlen = len(ln)
+                    if ln[lnlen - 1] == r"[":
+                        key = ln.lstrip(r"/")
+                        key = key.rstrip(r" [")
+                        data = {}
+                        lastData[key] = data
+                        stack.append(data)
+                        continue
+                    attrs = ln.split(r" ")
+                    key = attrs[0].lstrip(r"/")
+                    value = parseList(attrs[1:])
+                    lastData[key] = value
+                if ln == ">>":
+                    stack = stack[0:stackLen - 1]
+            return ed
+        else:
+            return None
+    except Exception,e:
+        #print "parseEngineData error: " + e.message
+        return None
+
+def getLabelSize(layer):
+    assert isLabel(layer)
+    try:
+        ed = parseEngineData(layer)
+        if ed is None:
+            return 18
+        else:
+            return int(ed["EngineDict"]["StyleRun"]["RunArray"]["StyleSheet"]["StyleSheetData"]["FontSize"])
+    except Exception,e:
+        return 18
+
+def getLabelColor(label):
+    assert isinstance(label,Layer) and label.text_data is not None
+    try:
+        ed = parseEngineData(label)
+        if ed is None:
+            return "0xffffff", 1
+        else:
+            colorInfo = ed["EngineDict"]["StyleRun"]["RunArray"]["StyleSheet"]["StyleSheetData"]["FillColor"]["Values"]
+            a, r, g, b = colorInfo[0], colorInfo[1], colorInfo[2], colorInfo[3]
+            return rgbToHex(round(float(r) * 255), round(float(g) * 255), round(float(b) * 255)), a
+    except Exception,e:
+        return "0xffffff", 1
+
+def getListTupleAttr(lst,key):
+    for l in lst:
+        if l[0].strip() == key:
+            return l[1]
+    return None
+
+def getLabelStrokeInfo(label):
+    assert isinstance(label,Layer) and label.text_data is not None
+    try:
+        #家里ps的版本
+
+        # info = label._tagged_blocks["lfx2"][2][2]
+        # for item in info:
+        #     if item[0] == "FrFX":
+        #         strokeInfo = item[1][2]
+        #         enabled = strokeInfo[0][1][0]
+        #         size = strokeInfo[5][1][1]
+        #         clr = strokeInfo[6][1][2]
+        #         r,g,b = clr[0][1][0],clr[1][1][0],clr[2][1][0]
+        #         #print enabled,size,r
+        #         return enabled,size,rgbToHex(round(r),round(g),round(b))
+        # return False,0,"0xffffff"
+
+        #公司的ps版本
+        info = label._tagged_blocks["lfx2"][2][2]
+        for item in info:
+            if item[0] == "FrFX":
+                frfx = item[1][2]
+                enabled = getListTupleAttr(frfx,"enab")[0] if getListTupleAttr(frfx,"enab") is not None else False
+                size = getListTupleAttr(frfx,"Sz")[1] if getListTupleAttr(frfx,"Sz") is not None else 0
+                clr = getListTupleAttr(frfx,"Clr")[2]
+                if clr is not None:
+                    r, g, b = clr[0][1][0], clr[1][1][0], clr[2][1][0]
+                else:
+                    r,g,b, = 0,0,0
+                return enabled, size, rgbToHex(round(r), round(g), round(b))
+        return False, 0, "0xffffff"
+
+    except Exception,e:
+        return False,0,"0xffffff"
+
+def getLabelText(layer):
+    """
+    获取label文字
+    :param layer:
+    :return:
+    """
+    if not isLabel(layer):
+        return ""
+    try:
+        tex = layer.text_data.text
+        # tex = tex.replace("\n","")
+        return tex
+    except:
+        return "unknown"
+
+def getLayerCustomProp(layer):
+    """
+    获取图层自定义属性
+    name 副本 :k=v;k=v;
+    :param layer:
+    :return:
+    """
+    ret = {}
+
+    names = layer.name.split(r":")
+    if len(names) > 1:
+        attrStr = names[-1]
+        if not re.search(r"=", attrStr):
+            return ret
+        kvList = attrStr.split(";")
+        for item in kvList:
+            k,v = item.strip().split(r"=")
+            ret[k.strip()] = v.strip()
+
+    return ret
+
+def getLayerProp(layer,isButton=False):
+    """
+    获得图层属性
+    自定义属性权重高于原始属性
+    :param layer:
+    :return:
+    """
+    ret = OrderedDict()
+    customProp = getLayerCustomProp(layer)
+    x, y, w, h = getLayerSize(layer)
+    ret["x"] = x
+    ret["y"] = y
+    ret["width"] = w
+    ret["height"] = h
+
+    if customProp.has_key("id"):
+        ret["id"] = customProp["id"]
+        del customProp["id"]
+    if customProp.has_key("name"):
+        ret["name"] = customProp["name"]
+        del customProp["name"]
+    if isButton and customProp.has_key("id"):
+        customProp["name"] = customProp["id"]
+
+    visible = getLayerVisible(layer)
+    if not visible:
+        ret["visible"] = "false"
+    alpha = getLayerAlpha(layer)
+    if alpha != 1:
+        ret["alpha"] = alpha
+
+    if isButton:
+        ret["touchEnabled"] = "true"
+    else:
+        ret["touchEnabled"] = "false"
+
+    if isGroup(layer):
+        ret["touchChildren"] = "false"
+
+    if isLabel(layer):
+        ret["fontFamily"] = "Microsoft YaHei"
+        ret["text"] = getLabelText(layer)
+        se, sz, sc = getLabelStrokeInfo(layer)
+        if se:
+            ret["stroke"] = int(sz)
+            ret["strokeColor"] = sc
+        ret["size"] = getLabelSize(layer)
+        color, alpha = getLabelColor(layer)
+        ret["textColor"] = color
+        if float(alpha) != float(1):
+            ret["alpha"] = alpha
+    elif isLayer(layer):
+        name = getLayerName(layer)
+        ret["source"] = "{}_png".format(name)
+
+    for k in customProp:
+        ret[k] = customProp[k]
+
+    return ret
 
 
 
 
 
 def main(argv):
-    pass
+    p = r"F:\work\n5\roll\art\psd\panel\mailPanel\MailContent.psd"
+    psd = PSDImage.load(p)
+    print ""
 
 
 if __name__ == '__main__':
